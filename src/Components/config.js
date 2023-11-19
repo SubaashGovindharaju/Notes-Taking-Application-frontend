7af8f1e2b05f3e984a36494001b37c3f92853824
Subaash G <104092882+SubaashGovindharaju@users.noreply.github.com>
2023-11-19 14:58:02 +0530

    add _redirects and backend url and changed the title

--- src/Components/config.js
+++ src/Components/config.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line no-undef
-// export const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://dietsuggestionapp-backend.onrender.com/api';
-export const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000/api';
+export const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://notes-taking-application-backend-l19a.onrender.com/api';
+// export const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000/api';
